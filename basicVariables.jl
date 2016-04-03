5e395e34998df7e9f272adbe0dc30ffb1010db9f
Robert <robdebruyn@gmail.com>
2016-04-03 01:39:11 -0400

    First Commit
    Just some basic files

---
+++ basicVariables.jl
@@ -0,0 +1,16 @@
+x = 4
+println(x)
+y = 5
+println(y)
+println("z = x + y")
+z = x + y
+println(z)
+println("w = x - y")
+w = x - y
+println(w)
+println("v = x / y")
+v = x / y
+println(v)
+println("u = x * y")
+u = x * y
+println(u)
