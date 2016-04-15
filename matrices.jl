1bc0bb96b75fed7f4f0c27a605382f85f99bb79e
Robert <robdebruyn@gmail.com>
2016-04-15 11:14:14 -0400

    Actually Doing Matrices
    Temporarily gave up on reading in the matrix from a file, just hard
    coded it.
    But now we can actually see what we want to be doing works

---
+++ matrices.jl
@@ -0,0 +1,19 @@
+#= I figured that since the actual project is about multiplying matrices
+I will just do that then worry about reading them in later =#
+
+mat1 = [1 2; 3 4]
+
+print("Matrix 1 \n", mat1, "\n\n")
+
+mat2 = [4 3; 2 1]
+
+print("Matrix 2 \n",  mat2, "\n\n")
+
+print("Multiplication \n")
+
+mat3 = mat1*mat2
+
+print(mat3, "\n")
+
+#= okay so that worked very well, super simple.
+  Now we can get into the good stuff=#
