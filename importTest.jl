55936c175e5d331a99b0540659e47ec100ba8e09
Robert <robdebruyn@gmail.com>
2016-04-16 12:32:25 -0400

    Successfully loaded the module

--- importTest.jl
+++ importTest.jl
@@ -1,4 +1,10 @@
-include(rand55.jl)
+
+#= This line is used to show Julia where to look for modules it may need to import
+   If other directories need to be searched, or this one need to be changed, just
+   change this line to reflect the desired directory=#
+   
+push!(LOAD_PATH, "/Users/Rob/GitHub/JuliaProject")
+using rand55
 
 m1 = makeMat(20)
 
